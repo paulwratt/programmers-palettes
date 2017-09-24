67bf42ee25935cdfe6c730dc73d4fd309a49be01
Paul Wratt <paul.wratt@gmail.com>
2017-09-25 07:52:07 +1300

    updated: minor changes

--- bin/as-dec-2-binary.sh
+++ bin/as-dec-2-binary.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 while [ $# -ne 0 ]; do
   DecNum=$1
-  Binary=
-  if [ $DecNum -eq 0 ]; then Binary=0; fi
+  Binary=""
+  if [ $DecNum -eq 0 ]; then Binary="0"; fi
   while [ $DecNum -ne 0 ]; do
     Bit=$(( $DecNum % 2 ))
-    Binary=$Bit$Binary
+    Binary="$Bit$Binary"
     DecNum=$(( $DecNum / 2 ))
   done
-  L=`echo "$Binary" | wc -c`
-  if [ $L -lt 8 ]; then
+  L=`echo "$Binary" | wc -m`
+  if [ $L -lt 9 ]; then
     for J in `seq $L 8`; do
       Binary="0$Binary"
     done
